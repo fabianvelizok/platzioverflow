f28ab80f3140adff828c7bc14bb35c97f93335e7
Fabián Veliz <velizfabianhoracio@gmail.com>
2018-04-08 03:41:05 -0300

    Add question routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { SigninComponent } from './auth/signin/signin.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { QuestionListComponent } from './question/question-list/question-list.component';
+import { QUESTION_ROUTES } from './question/question.routes';
 
 const ROUTES: Routes = [
   { path: '', component: QuestionListComponent, pathMatch: 'full' },
   { path: 'signin', component: SigninComponent },
   { path: 'signup', component: SignupComponent },
+  { path: 'questions', children: QUESTION_ROUTES },
 ];
 
 const router = RouterModule.forRoot(ROUTES);
