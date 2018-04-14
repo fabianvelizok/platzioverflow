dca520ac5672fa023f4bb126c8a1bfbdf2c2c28f
Fabián Veliz <velizfabianhoracio@gmail.com>
2018-04-13 23:50:33 -0300

    Prepare server to register new users

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -19,23 +19,29 @@ const fakeUsers = new Array(10).fill(fakeUser);
 const findUserByEmail = (email) => {
   const currentUser = fakeUsers.find(user => user.email === email);
   return currentUser;
-}
+};
 
 const comparePasswords = (enteredPassword, userPassword) => {
   return enteredPassword === userPassword;
-}
+};
 
 const handleLoginError = (res) => {
   return res.status(401).json({
     message: 'Login failed.',
     error: 'Wrong email or password.'
   });
-}
+};
 
+const generateToken = (user) => {
+  const secret = 'secret'; // I'll move this soon.
+  const token = jwt.sign({ user }, secret, { expiresIn: 86400 });
+  return token;
+};
+
+// POST /api/auth/signin
 app.post('/signin', (req, res) => {
   const { email, password } = req.body;
   const user = findUserByEmail(email);
-  const secret = 'secret'
 
   if (!user) {
     debug(`User with email: ${email} not found.`);
@@ -50,9 +56,40 @@ app.post('/signin', (req, res) => {
   }
 
   // Generate token
-  const token = jwt.sign({ user }, secret, { expiresIn: 86400 });
+  const token = generateToken(user);
+
   res.status(200).json({
     message: 'Login succeded',
+    token,
+    user: {
+      _id: user._id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+    }
+  });
+});
+
+// POST /api/auth/signup
+app.post('/signup', (req, res) => {
+  const { email, password, firstName, lastName } = req.body;
+
+  const user = {
+    _id: new Date.getTime(),
+    email,
+    password,
+    firstName,
+    lastName,
+  };
+
+  fakeUsers.push(user);
+
+  debug(`User created successfully`);
+
+  const token = generateToken(user);
+
+  res.status(201).json({
+    message: 'Signup succeded',
     token,
     user: {
       _id: user._id,
